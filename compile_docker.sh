e0827dce64716c58b0946a771f14249e0b20b73a
teckwan <wong.teck.wan@outlook.com>
2020-03-10 18:59:27 +0100

    Fix for compilation

--- compile_docker.sh
+++ compile_docker.sh
@@ -1,1 +1,1 @@
-docker run --rm -v $(pwd):/work eguerin/antlr4cpp bash -c "cd /work; make ANTLR4_INCDIR=/usr/include/antlr4-runtime ANTLR4_BINDIR=/bin ANTLR4_LIBDIR=/lib/x86_64-linux-gnu"
+docker run --rm -v $(pwd):/work eguerin/antlr4cpp bash -c "cd /work; make ANTLR4_INCDIR=/usr/include/antlr4-runtime ANTLR4_BIN=/bin/antlr4 ANTLR4_LIB=/lib/x86_64-linux-gnu/libantlr4-runtime.a"
--- compile_ubuntu.sh
+++ compile_ubuntu.sh
@@ -1,1 +1,1 @@
-make ANTLR4_BINDIR=/usr/bin  ANTLR4_INCDIR=/usr/local/include/antlr4-runtime ANTLR4_LIBDIR=/usr/local/lib
+make ANTLR4_BIN=/usr/bin/antlr4  ANTLR4_INCDIR=/usr/local/include/antlr4-runtime ANTLR4_LIB=/usr/local/lib/libantlr4-runtime.a
